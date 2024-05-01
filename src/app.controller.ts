6f491290789a7f44254fdd21dc9aa90fae9a5f29
npx <xuannhutzz@gmail.com>
2024-05-01 22:20:06 +0700

    setup jwt

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,24 +1,20 @@
-import { Controller, Get, Render } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
-  @Get()
-  // getHello(): string {
-  //   return this.appService.getHello();
-  // }
-  @Render('home')
-  getHello() {
-    console.log(
-      '==> Server run Port: ',
-      this.configService.get<string>('PORT'),
-    );
-    return { message: 'Hello world' };
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
-import { PassportModule } from '@nestjs/passport';
+import { PassportModule, PassportStrategy } from '@nestjs/passport';
 import { LocalStrategy } from './passport/local.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [
+    UsersModule,
+    PassportModule,
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          secret: configService.get<string>('JWT_KEY'),
+          signOptions: { expiresIn: '60s' },
+        };
+      },
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
