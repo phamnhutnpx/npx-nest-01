736fb7d6e26dfb73b23f3d4e6983b7580d9c2a46
nhutisd <nhut.px@isdcorp.vn>
2024-05-03 21:39:44 +0700

    accept cors

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ import { JwtAuthGuard } from './auth/jwt-auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const reflector = app.get(Reflector);
-  app.useGlobalGuards(new JwtAuthGuard(reflector));
+  // app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
@@ -17,6 +17,10 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+  });
   await app.listen(port);
 }
 bootstrap();
