b4d548a2fc30cf439009c387837610b5484def14
devnhut <xuannhutzz@gmail.com>
2024-05-05 17:01:30 +0700

    update create by when add company

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -4,6 +4,7 @@ import { UpdateCompanyDto } from './dto/update-company.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Company, CompanyDocument } from './schema/company.schema';
 import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
+import { IUser } from 'src/users/users.interface';
 
 @Injectable()
 export class CompaniesService {
@@ -12,8 +13,14 @@ export class CompaniesService {
     private companyModel: SoftDeleteModel<CompanyDocument>,
   ) {}
 
-  async create(createCompanyDto: CreateCompanyDto) {
-    const createdCompany = await this.companyModel.create(createCompanyDto);
+  async create(createCompanyDto: CreateCompanyDto, user: IUser) {
+    const createdCompany = await this.companyModel.create({
+      ...createCompanyDto,
+      createdBy: {
+        _id: user._id,
+        email: user.email,
+      },
+    });
     return createdCompany;
   }
 
