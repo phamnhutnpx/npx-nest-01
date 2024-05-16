56a8f96776bd0031053799b52621e78dbf1366a2
devnhut <nhut.px@isdcorp.vn>
2024-05-16 22:46:07 +0700

    config interceptor

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,14 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { TransformInterceptor } from './core/transform.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const reflector = app.get(Reflector);
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalInterceptors(new TransformInterceptor());
   const configService = app.get(ConfigService);
   const port = configService.get<string>('PORT');
 
