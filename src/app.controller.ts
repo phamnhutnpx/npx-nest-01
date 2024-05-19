d0612a0cc39b7fa42f46858d60e4a689eeb6e7e0
devnhut <nhut.px@isdcorp.vn>
2024-05-19 08:52:07 +0700

    show init app

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,28 +1,12 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Controller, Get, Request } from '@nestjs/common';
 import { AppService } from './app.service';
-import { LocalAuthGuard } from './auth/local-auth.guard';
-import { AuthService } from './auth/auth.service';
-import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { Public } from './decorator/customize';
 @Controller()
 export class AppController {
-  // constructor(
-  //   private readonly appService: AppService,
-  //   private configService: ConfigService,
-  //   private authService: AuthService,
-  // ) {}
-
-  // @Public()
-  // @UseGuards(LocalAuthGuard)
-  // @Post('auth/login')
-  // async login(@Request() req) {
-  //   return this.authService.login(req.user);
-  // }
-
-  // @UseGuards(JwtAuthGuard)
-  @Get('profile')
-  getProfile(@Request() req) {
-    return req.user;
+  constructor(private readonly appService: AppService) {}
+  @Public()
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
